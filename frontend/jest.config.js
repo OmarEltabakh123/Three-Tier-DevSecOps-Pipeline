bee294e9e2589d42569c7161ffc91fd1eeac71a8
OmarEltabakh123 <omar.ahmed.attia123@gmail.com>
2025-06-03 20:40:21 +0300

    Fixed build issue by adding openssl legacy provider option

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  transform: {},
-  testEnvironment: 'jsdom',
-  extensionsToTreatAsEsm: ['.js', '.jsx'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
-}
-
